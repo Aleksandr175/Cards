8a6ae1883c2878be3e53c12c7f49459bac36119d
Aleksandr <a.pogr@yandex.ru>
2013-03-13 23:10:20 +0400

    Обработчик на кнопке "Угадай"
    Переход на второй экран

--- src/ru/alexdev/cards/MainActivity.java
+++ src/ru/alexdev/cards/MainActivity.java
@@ -2,14 +2,22 @@ package ru.alexdev.cards;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button buttonGameGuess;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        buttonGameGuess = (Button) findViewById(R.id.button_game_guess);
+        listenerSelectGameGuess();
     }
 
 
@@ -18,6 +26,23 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    /**
+     * Обработчик нажатия на кнопку "Угадай"
+     * 
+     * @return void
+     */
+    private void listenerSelectGameGuess() 
+    {
+    	buttonGameGuess.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setContentView(R.layout.activity_game_guess);
+			}
+		});
     }
     
 }
